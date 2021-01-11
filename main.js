0a95662adb2cba3b035f25656aaf48c543d0397d
yassine issifi <yassineissifi@gmail.com>
2021-01-11 16:33:47 +0100

    add the switch player methode

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 // factory player
-const player = (boardMark) => {
+const player = (playerName, boardMark) => {
+    let name = playerName;
     const getMark = () => boardMark;
+    const getName = () => name;
+    const setName = (newName) => name = newName;
+    return { getMark, getName, setName }
 };
-// the module 
+
+
+// the module for controle
+
+const game = (() => {
+    const player1 = player('player1', 'X');
+    const player2 = player('player2', 'O');
+    let playerTurn = player1;
+    const getPlayerTurn = () => playerTurn;
+    const switchPlayerTurn = () => {
+        if(playerTurn.getName() === 'player1'){
+            playerTurn = player2;
+        }else {
+            playerTurn = player1;
+        }
+    }
+    return { player1, player2, getPlayerTurn, switchPlayerTurn}
+})();
+
+
+// the module for the game bord
 const gameBord = (()=>{
-    let gameBord = ['','x','','o','','x','','o','x'];
+    let gameBord = ['','','','','','','','',''];
     // cache DOM
     let gameBordGrid = document.querySelector('#grid-container');
     let player1 = document.querySelector('#player-1');
@@ -17,9 +41,16 @@ const gameBord = (()=>{
             gameBord.forEach(draw)
         }
     }
-    const addMark = () => {
-
+    const addMark = (item, index)=>{
+        item.addEventListener('click', ()=>{
+            if(gameBord[index] === ''){
+                gameBord[index] = game.getPlayerTurn().getMark();
+                game.switchPlayerTurn()
+            }
+            render();
+        })
     }
+
     const draw = (item, index) => {
         gameBordBtn[index].textContent = item
     }
@@ -32,12 +63,5 @@ const gameBord = (()=>{
     // btn events 
     newBtn.addEventListener('click', clearGameBord);
 
-    gameBordBtn.forEach((item, index)=>{
-        item.addEventListener('click', ()=>{
-            if(gameBord[index] === ''){
-                gameBord[index] = 'x';
-            }
-            render();
-        })
-    })
+    gameBordBtn.forEach(addMark);
 })();
